07d62ae800235444c72863e3d27c77c3754d3f11
WillVegapunk <willvegapunk@gmail.com>
2017-07-20 06:58:59 +0000

    cscope coredump ctags

--- hw0/wc.c
+++ hw0/wc.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]) {
+    FILE *fp;
+    fp = fopen(argv[1], "r");
+    char tmp;
+    unsigned long long line_count = 0, wordcount=0;
+//    while( fscanf(fp, "%c", &tmp)==1 ){ // this works
+    while( (tmp = fgetc(fp)) != EOF ){
+	printf("%c",tmp);
+	if (tmp == '\n')
+	    line_count++;
+	else
+            wordcount++;
+    }
+    printf("line count: %lld, word count : %lld\n", line_count, wordcount);
+    fclose(fp);
     return 0;
 }
